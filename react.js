12ff2baa2e2d5b2783353997e83e5f87360222e4
mattperkins <gthb_1@dormshed.com>
2020-03-03 21:41:46 +0000

    Update react.js

--- react.js
+++ react.js
@@ -1,6 +1,6 @@
 class ColorChecker extends React.Component{
-                        constructor(props, context){
-                    super(props, context)
+   constructor(props, context){
+    super(props, context)
 
             this.state= {
                 bgColor: "#887766"
@@ -34,26 +34,26 @@ class ColorChecker extends React.Component{
 
             return (
                 <div className="colorArea">
-                        <div style={squareStyle} className="colorCircle"></div>
+                  <div style={squareStyle} className="colorCircle"></div>
 
-                        <form onSubmit={this.setNewColor}>
-                            <input onChange={this.colorValue}
-                                    ref={
-                                        (el) => this._input = el
-                                    }
-                                placeholder="Enter a color value"></input>
-                            <button type="submit">Go</button>
-                        </form>
+                  <form onSubmit={this.setNewColor}>
+                      <input onChange={this.colorValue}
+                              ref={
+                                  (el) => this._input = el
+                              }
+                          placeholder="Enter a color value"></input>
+                      <button type="submit">Go</button>
+                  </form>
 
-                        <div className="instructions">
-                        <h3>Examples:</h3>
-                        <p>Hex Colour = <strong>#ffff00</strong></p>
-                        <p>Colour Name = <strong>red</strong></p>
-                        <p>RGBA = <strong>rgba(1,255,100,.5)</strong></p>
-                        </div>  
+                  <div className="instructions">
+                  <h3>Examples:</h3>
+                  <p>Hex Colour = <strong>#ffff00</strong></p>
+                  <p>Colour Name = <strong>red</strong></p>
+                  <p>RGBA = <strong>rgba(1,255,100,.5)</strong></p>
+                  </div>  
 
-                        <ColorLabel color={this.state.bgColor} />
-                    </div>
+                  <ColorLabel color={this.state.bgColor} />
+              </div>
                     )
         }
     }
